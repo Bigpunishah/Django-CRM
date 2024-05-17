3df68852fdb701023a12e257008f74c6af47bef2
Jay C <vikel965@hotmail.com>
2024-05-17 13:10:01 -0400

    Final portions completed for CRM - CRUD operations accessible completely now.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from.models import Record
 
 # Create a dir named `templates` to add .html files
@@ -66,6 +66,54 @@ def customer_record(request, primary_key):
         # Look up record
         customer_record = Record.objects.get(id = primary_key)
         return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "Must be logged in!")
+        return redirect('home')
+    
+
+def delete_record(request, primary_key):
+    # If user is auth then...
+    if request.user.is_authenticated:
+        # Find record from the primary key
+        delete_it = Record.objects.get(id=primary_key)
+        # Delete record
+        delete_it.delete()
+        messages.success(request, "Record has been deleted")
+        return redirect('home')
+    else:
+        messages.success(request, "Must be logged in!")
+        return redirect('home')
+    
+
+# Adding a record
+def add_record(request):
+    # Creating a new form
+    form = AddRecordForm(request.POST or None)
+    # If user is auth then...
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            # Ensure its valid
+            if form.is_valid():
+                # Save form
+                form.save()
+                messages.success(request, "Record Added!")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "Must be logged in!")
+        return redirect('home')
+    
+
+def update_record(request, primary_key):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=primary_key)
+        # This places all saved values in the the current fields
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
     else:
         messages.success(request, "Must be logged in!")
         return redirect('home')
